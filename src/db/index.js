d652d61fdf0733845817e02fe03e40c6a49c78e1
Tushika <tushikagarg11@gmail.com>
2024-02-03 19:47:54 +0530

    mongo db atlas connection

--- src/db/index.js
+++ src/db/index.js
@@ -1,15 +1,19 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 
-const connectDB=async()=>{
-    try{
-        const connectionInstance=await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
-        console.log(`DB Connected!! DB Host: ${connectionInstance.connection.host}`);
-    }catch(error){
-        console.log("MONGODB connection failed: ", error);
-        //node js gives you the access of 'process' which refers to the current application(your current application must be running on some process and this 'process' is the reference to it)
-        process.exit(1);
-    }
-}
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URL}/${DB_NAME}`
+    );
+    console.log(
+      `MONGODB connected successfully!! DB Host: ${connectionInstance.connection.host}`
+    );
+  } catch (error) {
+    console.log(`MONGODB Connection failed: ${error}`);
+    //node js gives you the access of 'process' which refers to the current application(your current application must be running on some process and this 'process' is the reference to it)
+    process.exit(1);
+  }
+};
 
-export default connectDB
+export default connectDB;
